3ff633b3fce146bd479bd13ad1fc0ae86136be37
ignaciosolis <ignacio.solis@ug.uchile.cl>
2017-05-15 13:16:06 -0400

    segunda subida - finals

--- about.php
+++ about.php
@@ -7,7 +7,8 @@ array_walk($datos, function(&$a) use ($datos) {$a = array_combine($datos[0], $a)
 array_shift($datos);
 ?>
 <h2>Sobre el proyecto</h2>
-<p>El tema aborda el análisis de los estereotipos de géneros como ente articulador en los imaginarios expuestos en la sociedad, donde a través de los medios ópticos y el fenómeno de la irradiación de luz en la cámara permita exponer lo discriminado y marginado de las ficciones de identidad.</p>
+<p>El tema aborda el análisis de los estereotipos de géneros como ente articulador en los imaginarios expuestos en la sociedad, donde a través de los medios ópticos y el fenómeno de la irradiación de luz en la cámara permita exponer lo discriminado y marginado de las ficciones de identidad.
+Principalmente el tema de investigación se centra en el concepto de travestismo marcado por la marginalidad y el desajuste corporal, donde se ahondará en como estos cuerpos ficcionados toman un discurso que no les es propio, como por ejemplo el de la Iglesia Católica, y a través de la performance se genera un acto de disidencia y reivindicación política por medio de la imagen. </p>
 
 </section>
 <?php include('pie.php');?>
--- cabeza.php
+++ cabeza.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <?php
 $title = "Ensoñaciones y ficciones de identidad";
-$descripcion = "Pongan acá su descripción.";
+$descripcion = "Travestismo como ente articulador de los imaginarios colectivos.";
 ?>
 <head>
 <meta charset="utf-8">
@@ -10,7 +10,7 @@ $descripcion = "Pongan acá su descripción.";
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php echo($title);?></title>
 <!--acá vamos a meter varios metadatos-->
-<meta name="robots" content="noindex">
+<meta name="description" content="<?php echo($descripcion);?>">
 <link href="https://fonts.googleapis.com/css?family=Archivo+Black" rel="stylesheet">
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <link href="bootstrap/style.css" rel="stylesheet">
@@ -30,14 +30,10 @@ $descripcion = "Pongan acá su descripción.";
 <nav>
 <ul class="nav masthead-nav">
 <li<?php if((basename($_SERVER['PHP_SELF']))=='index.php'){?> class="active" <?php };?>><a href="index.php">Portada</a></li>
+<li<?php if((basename($_SERVER['PHP_SELF']))=='datosamigos.php'){?> class="active" <?php };?>><a href="datosamigos.php">Datos Amigos</a></li>
 <li<?php if((basename($_SERVER['PHP_SELF']))=='about.php'){?> class="active" <?php };?>><a href="about.php">Sobre el proyecto</a></li>
 </ul>
 </nav>
 </div>
 </div>
-<?php if((basename($_SERVER['PHP_SELF']))=='index.php'){?>
-  <h2><?php echo($descripcion_index);?></h2>
-<?php }else{ ?>
-  <h2><?php echo($descripcion_estudiantes);?></h2>
-<?php };?>
 </header>
